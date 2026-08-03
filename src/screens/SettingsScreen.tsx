2c27b5e822aa1acbc1a23cd874160aedc38bc4d1
lys0318 <tosca0318@gmail.com>
2026-08-03 21:12:25 +0900

    chore: 1.3.6/vc54

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -561,7 +561,7 @@ export default function SettingsScreen() {
       {/* 앱 정보 */}
       <View style={styles.appInfo}>
         <Text style={styles.appName}>하우웨더유</Text>
-        <Text style={styles.appVersion}>v1.3.5</Text>
+        <Text style={styles.appVersion}>v1.3.6</Text>
       </View>
       </ScrollView>
       <ProfileEditor visible={profileOpen} onClose={() => setProfileOpen(false)} />
